5134c914c6a8cc85e18da4935bcb9df44ec43337
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2023-11-25 17:18:20 +0530

    new counter

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,14 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [counter, setCount] = useState(0)
+  const plusValue = ()=>{
+    setCount(counter+1);
+    console.log("clicked"+counter);
+  }
 
+  const minus = ()=>{
+    setCount(counter-1);
+  }
   return (
     <>
       <h1>Chai aur react</h1>
-      <h2>count: 0</h2>
-      <button>+</button>
-      <button>-</button>
+      <h2>count: {counter}</h2>
+      <button onClick={plusValue}>+</button>
+      <button onClick={minus}>-</button>
     </>
   )
 }
